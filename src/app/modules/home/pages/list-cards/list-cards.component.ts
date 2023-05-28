ace97d065e2187bd563cea10a099190b9e393932
Camila Sales <camila.07ramones@gmail.com>
2023-05-28 20:48:16 -0300

    feat: adiciona imagens das cartas e valores

--- src/app/modules/home/pages/list-cards/list-cards.component.ts
+++ src/app/modules/home/pages/list-cards/list-cards.component.ts
@@ -10,7 +10,7 @@ export class ListCardsComponent {
     {
       titulo:'Alimentação Saudável',
       descricao: 'Você achou a carta de hábitos alimentares saudáveis, hora de comer, a cada carta jogada uma fruta ou vegetal comida',
-      urlImagem: '',
+      urlImagem: '../../../../../assets/icons/alimentacao.png',
       valor1: '560',
       valor2: '250',
       color: '#fef3b1',
@@ -18,7 +18,7 @@ export class ListCardsComponent {
     {
       titulo:'Cultivo em Horta',
       descricao: 'Você achou a carta do cultivo em horta, hora de plantar, a cada carta jogada, uma planata plnatada',
-      urlImagem: '',
+      urlImagem: '../../../../../assets/icons/cultivo.png',
       valor1: '680',
       valor2: '490',
       color: '#e8ffcb',
@@ -26,7 +26,7 @@ export class ListCardsComponent {
     {
       titulo:'Separação de Lixo',
       descricao: 'Você achou a carta de separação de lixo, hora de separar, a cada carta jogada um lixo separado',
-      urlImagem: '',
+      urlImagem: '../../../../../assets/icons/separacao.png',
       valor1: '806',
       valor2: '360',
       color: '#feb2b2',
@@ -34,7 +34,7 @@ export class ListCardsComponent {
     {
       titulo:'Prática de Atividade Física',
       descricao: 'Você achou a carta de práticas de atividades fisicas, hora de pedalar, a cada carta jogada 1km pedalado',
-      urlImagem: '',
+      urlImagem: '../../../../../assets/icons/atividades.png',
       valor1: '806',
       valor2: '360',
       color: '#ffd2b1',
