1b36c3fb2f4c23167fc85429ac1da3fc608a1461
Camila Sales <camila.07ramones@gmail.com>
2023-06-11 20:17:54 -0300

    fix: corrige rota api

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://my-api-url'
+    apiUrl: 'https://esg-adventure-api.onrender.com'
+    // apiUrl: 'http://localhost:3000'
 };
